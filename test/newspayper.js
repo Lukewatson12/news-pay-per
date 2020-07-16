190f014d54c11741aec2b37fb1683dc0c8f81012
Luke Watson <>
2020-07-16 16:35:35 +0800

    uncomment tests

--- test/newspayper.js
+++ test/newspayper.js
@@ -17,95 +17,95 @@ contract("NewsPayPer", accounts => {
             });
     });
 
-    // it("Should allow creation and retrieval of an article", async () => {
-    //     let articleDescription = "This is my article name";
-    //
-    //     await newsPayPerInstance.addArticle(
-    //         articleDescription,
-    //         oneEther,
-    //         {from: alice}
-    //     );
-    //
-    //     const article = await newsPayPerInstance.getArticle.call(1);
-    //     const {0: description, 1: cost} = article;
-    //
-    //     assert.equal(
-    //         description,
-    //         articleDescription,
-    //         "Description does not match"
-    //     );
-    //
-    //     assert.equal(
-    //         cost.toString(),
-    //         oneEther,
-    //         "Unable to find the price for the article"
-    //     );
-    // });
-    //
-    // it("It should store multiple articles and return a list when queried", async () => {
-    //     let articleDescription = "This is my article name";
-    //
-    //     await newsPayPerInstance.addArticle(
-    //         articleDescription,
-    //         oneEther,
-    //         {from: alice}
-    //     );
-    //
-    //     await newsPayPerInstance.addArticle(
-    //         articleDescription,
-    //         oneEther,
-    //         {from: alice}
-    //     );
-    //
-    //     await newsPayPerInstance.addArticle(
-    //         articleDescription,
-    //         twoEther,
-    //         {from: alice}
-    //     );
-    //
-    //     const articles = await newsPayPerInstance.getArticles.call();
-    //
-    //     assert.equal(
-    //         articles.length,
-    //         3,
-    //         "Total number of articles does not match total saved"
-    //     );
-    // });
-    //
-    // it("Should allow a wallet to purchase an article", async () => {
-    //     let articleDescription = "This is my article name";
-    //
-    //     await newsPayPerInstance.addArticle(
-    //         articleDescription,
-    //         oneEther,
-    //         {from: alice}
-    //     );
-    //
-    //     await newsPayPerInstance.purchaseArticle(
-    //         1,
-    //         {
-    //             value: oneEther,
-    //             from: bob
-    //         }
-    //     );
-    //
-    //     await newsPayPerInstance.hasArticle(
-    //         1,
-    //         {from: alice}
-    //     ).then(hasArticle => assert.equal(
-    //         false,
-    //         hasArticle,
-    //         "Alice has not purchased the article but owns it"
-    //     ));
-    //
-    //     await newsPayPerInstance.hasArticle(
-    //         1,
-    //         {from: bob}
-    //     ).then(hasArticle => assert.ok(
-    //         hasArticle,
-    //         "Bob has purchased the article but does not own it"
-    //     ));
-    // });
+    it("Should allow creation and retrieval of an article", async () => {
+        let articleDescription = "This is my article name";
+
+        await newsPayPerInstance.addArticle(
+            articleDescription,
+            oneEther,
+            {from: alice}
+        );
+
+        const article = await newsPayPerInstance.getArticle.call(1);
+        const {0: description, 1: cost} = article;
+
+        assert.equal(
+            description,
+            articleDescription,
+            "Description does not match"
+        );
+
+        assert.equal(
+            cost.toString(),
+            oneEther,
+            "Unable to find the price for the article"
+        );
+    });
+
+    it("It should store multiple articles and return a list when queried", async () => {
+        let articleDescription = "This is my article name";
+
+        await newsPayPerInstance.addArticle(
+            articleDescription,
+            oneEther,
+            {from: alice}
+        );
+
+        await newsPayPerInstance.addArticle(
+            articleDescription,
+            oneEther,
+            {from: alice}
+        );
+
+        await newsPayPerInstance.addArticle(
+            articleDescription,
+            twoEther,
+            {from: alice}
+        );
+
+        const articles = await newsPayPerInstance.getArticles.call();
+
+        assert.equal(
+            articles.length,
+            3,
+            "Total number of articles does not match total saved"
+        );
+    });
+
+    it("Should allow a wallet to purchase an article", async () => {
+        let articleDescription = "This is my article name";
+
+        await newsPayPerInstance.addArticle(
+            articleDescription,
+            oneEther,
+            {from: alice}
+        );
+
+        await newsPayPerInstance.purchaseArticle(
+            1,
+            {
+                value: oneEther,
+                from: bob
+            }
+        );
+
+        await newsPayPerInstance.hasArticle(
+            1,
+            {from: alice}
+        ).then(hasArticle => assert.equal(
+            false,
+            hasArticle,
+            "Alice has not purchased the article but owns it"
+        ));
+
+        await newsPayPerInstance.hasArticle(
+            1,
+            {from: bob}
+        ).then(hasArticle => assert.ok(
+            hasArticle,
+            "Bob has purchased the article but does not own it"
+        ));
+    });
 
     it("Should should not allow you to purchase the same article twice", async () => {
         let articleDescription = "This is my article name";
