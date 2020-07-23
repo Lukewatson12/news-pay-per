32c7e21fd7fdd0f452ec98c84507f472bbe3919e
Luke Watson <>
2020-07-23 17:25:48 +0800

    Clean up news component

--- app/src/News.js
+++ app/src/News.js
@@ -35,61 +35,6 @@ class News extends Component {
 
     }
 
-    pushArticle = article => {
-        const {drizzle, drizzleState} = this.props;
-        const contract = drizzle.contracts.NewsPayPer;
-
-        // let drizzle know we want to call the `set` method with `value`
-        const stackId = contract.methods["addArticle"].cacheSend(
-            article,
-            12,
-            {
-                from: drizzleState.accounts[0]
-            }
-        );
-
-        // save the `stackId` for later reference
-        this.setState({stackId});
-    };
-
-    getTxStatus = () => {
-        // get the transaction states from the drizzle state
-        const {transactions, transactionStack} = this.props.drizzleState;
-
-        // get the transaction hash using our saved `stackId`
-        const txHash = transactionStack[this.state.stackId];
-
-        // if transaction hash does not exist, don't display anything
-        if (!txHash) return null;
-
-        // otherwise, return the transaction status
-        return `Transaction status: ${transactions[txHash] && transactions[txHash].status}`;
-    };
-
-    // fetchArticle = (id) => {
-    //     const {drizzle, drizzleState} = this.props;
-    //     const contract = drizzle.contracts.NewsPayPer;
-    //     const state = drizzleState.contracts.NewsPayPer;
-    //
-    //     // If Drizzle is initialized (and therefore web3, accounts and contracts), continue.
-    //     if (state.initialized) {
-    //         const stackId = contract.methods.getArticle.cacheCall(id)
-    //         const ww = contract.methods.getArticles.cacheCall()
-    //         const myString = state.getArticle[stackId];
-    //         console.log(stackId);
-    //         console.log(state);
-    //
-    //         // let newArticles = [
-    //         //     ...state.articles,
-    //         //     {}
-    //         // ];
-    //         // this.setState({
-    //         //     newArticles
-    //         // })
-    //     }
-    //
-    // };
-
     render() {
         const {drizzleState} = this.props;
 
