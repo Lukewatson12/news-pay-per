8abb323c25d87d22e381faee44af3ab73d8c9ba6
Luke Watson <>
2020-07-26 21:51:32 +0800

    styling

--- app/src/component/Article/Article.js
+++ app/src/component/Article/Article.js
@@ -1,11 +1,29 @@
 import React, {useEffect, useState} from "react";
 import * as PropTypes from "prop-types";
-import Paper from "@material-ui/core/Paper";
 import {Link} from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import Card from "@material-ui/core/Card";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles({
+    media: {
+        height: 140,
+    },
+    card: {
+        "margin-bottom": 15,
+    },
+});
+
 
 const Article = (props) => {
     let {id, drizzle, drizzleState} = props;
     const [articleKey, setArticleKey] = useState(undefined)
+    const classes = useStyles();
 
     const newsPayPerContract = drizzle.contracts.NewsPayPer;
     const store = drizzleState.contracts;
@@ -23,13 +41,31 @@ const Article = (props) => {
     }
 
     return (
-        <Paper>
-            <h3>Article id is {id}</h3>
-            <h3>Article cost is {article.value[1]}</h3>
-            <Link to={"articles/" + id}>
-                Read article
-            </Link>
-        </Paper>
+        <Card className={classes.card}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    title="Article title"
+                    image={"https://www.blockassets.io/wp-content/uploads/2017/03/Background-1356x430.jpg"}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Article title
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        In tristique bibendum quam vitae hendrerit. Pellentesque lacus est, placerat aliquet interdum
+                        sed...
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Link to={"articles/" + id}>
+                    <Button size="small" color="primary">
+                        Read article
+                    </Button>
+                </Link>
+            </CardActions>
+        </Card>
     )
 }
 
