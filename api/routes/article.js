53b0ab5c03a4991b9a8ded07242abb69a58e8899
Luke Watson <>
2020-07-28 16:47:11 +0800

    Add endpoint to get article content

--- api/routes/article.js
+++ api/routes/article.js
@@ -10,17 +10,30 @@ const router = express.Router();
 router.get('/', function (req, res, next) {
     const coursesContract = new web3.eth.Contract(NewsPayPer.abi, "0xCF2b4642b9601c77B7C83a7BC73670d36d97D394");
 
-    web3.eth.getAccounts()
-        .then(accounts => accounts[0])
-        .then(
-            defaultAccount =>
-                coursesContract.methods.hasArticle(1).call({
-                    "from": defaultAccount
-                })
-        )
-        .then(
-            hasArticle => res.send(hasArticle)
-        )
+    getHasArticle()
+        .then(hasArticle => res.send(
+            getArticleContent(hasArticle)
+        ))
+
+    function getArticleContent(hasArticle) {
+        if (hasArticle) {
+            return "hello";
+        }
+
+        return "No article";
+    }
+
+    function getHasArticle() {
+        return web3.eth.getAccounts()
+            .then(accounts => accounts[0])
+            .then(
+                defaultAccount =>
+                    coursesContract.methods.hasArticle(1).call({
+                        "from": defaultAccount
+                    })
+            )
+    }
+
 });
 
 module.exports = router;
