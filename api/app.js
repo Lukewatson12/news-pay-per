b4ba78cf99c83d0675f496cecbfb97ef1c1aedb5
Luke Watson <>
2020-07-29 16:09:57 +0800

    Add sagas and connect to api

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,7 @@ const HOST = '0.0.0.0';
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
